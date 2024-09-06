075d75a04a2a038cc3db53964cd594e18de98899
Jio2453 <jiofast289@gmail.com>
2024-09-06 09:52:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7480308205:AAEAqT33JXmpwVOkp0K1wC5AhHb4bb6dhHo")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("radhe2453")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5904877352")
 
 
 
